f306a60e3c6691e8f79cf4ec2fee1359c1289b43
Jongwon Woo <jongwonwoo@sk.com>
2016-03-29 20:01:32 +0900

    Add tests

--- ElasticMotionTests/ElasticMotionTests.swift
+++ ElasticMotionTests/ElasticMotionTests.swift
@@ -9,37 +9,93 @@
 import XCTest
 @testable import ElasticMotion
 
-class ElasticMotionTests: XCTestCase {
+class ElasticMotionTests: XCTestCase, ElasticMotionStateMachineDelegate {
+    var stateMachine: ElasticMotionStateMachine?
+    let vibrationSec = 0.5
+    
+    var expectation: XCTestExpectation?
+    
+    let openToRight_MayOpen = "openToRight_MayOpen"
+    let openToRight_WillOpen = "openToRight_WillOpen"
+    let openToRight_Opened = "openToRight_Opened"
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        self.stateMachine = ElasticMotionStateMachine(ElasticMotionDirection.Right, threshold: 50.0, vibrationSec: vibrationSec)
+        self.stateMachine?.delegate = self
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.stateMachine?.delegate = nil
+        
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testOpenToRightMayOpen() {
+        self.expectation = expectationWithDescription(openToRight_MayOpen)
         
-        
-        //        //TODO: add elasticmotionstatemachine test
-        //        if let stateMachine = self.stateMachine {
-        //            stateMachine.setPoint(CGPointMake(0, 0), delta:CGPointMake(1, 0))
-        //            stateMachine.setPoint(CGPointMake(1, 0), delta:CGPointMake(1, 1))
-        //            stateMachine.setPoint(CGPointMake(2, 1), delta:CGPointMake(3, 1))
-        //            stateMachine.setPoint(CGPointMake(5, 0), delta:CGPointMake(1, 1))
-        //            stateMachine.setPoint(CGPointMake(6, 0), delta:CGPointMake(3, 1))
-        //            stateMachine.setPoint(CGPointMake(9, 0), delta:CGPointMake(1, 1))
-        //        }
+        if let stateMachine = self.stateMachine {
+            stateMachine.keepMoving(CGPointMake(0, 10))
+            
+            waitForExpectationsWithTimeout(0.1) { error in
+                if let error = error {
+                    XCTFail("Error: \(error.localizedDescription)")
+                }
+            }
+        }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testOpenToRightWillOpen() {
+        self.expectation = expectationWithDescription(openToRight_WillOpen)
+
+        if let stateMachine = self.stateMachine {
+            stateMachine.keepMoving(CGPointMake(0, 10))
+            stateMachine.keepMoving(CGPointMake(20, 10))
+            stateMachine.keepMoving(CGPointMake(30, 10))
+            stateMachine.keepMoving(CGPointMake(51, 10))
+            
+            waitForExpectationsWithTimeout(0.1) { error in
+                if let error = error {
+                    XCTFail("Error: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func testOpenToRightOpened() {
+        self.expectation = expectationWithDescription(openToRight_Opened)
+        
+        if let stateMachine = self.stateMachine {
+            stateMachine.keepMoving(CGPointMake(0, 10))
+            stateMachine.keepMoving(CGPointMake(20, 10))
+            stateMachine.keepMoving(CGPointMake(30, 10))
+            stateMachine.keepMoving(CGPointMake(51, 10))
+            
+            waitForExpectationsWithTimeout(vibrationSec * 1.01) { error in
+                if let error = error {
+                    XCTFail("Error: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    
+    func elasticMotionStateMachine(stateMachine: ElasticMotionStateMachine, didChangeState state: ElasticMotionState, deltaPoint: CGPoint) {
+        if (self.expectation?.description == openToRight_MayOpen) {
+            if (state == ElasticMotionState.MayOpen) {
+                self.expectation?.fulfill()
+            }
+        } else if (self.expectation?.description == openToRight_WillOpen) {
+            if (state == ElasticMotionState.WillOpen) {
+                self.expectation?.fulfill()
+            }
+        } else if (self.expectation?.description == openToRight_Opened) {
+            if (state == ElasticMotionState.Opened) {
+                self.expectation?.fulfill()
+            }
         }
     }
     
